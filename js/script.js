d9df953f086be47e51d8e6f47e936e20735ca4f1
EmanuelPerdomo <emaperdomo00@gmail.com>
2023-09-27 12:40:59 -0300

    Primer borrador de menú.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 //Calculadora de raices de un polinomio de segundo grado
-
-alert("Calculadora de raices de un polinomio de 2do grado de la forma ax^2 +/- bx +/- c");
-
-//¡Error: Admite espacios como entrada!!!
-function validarNumero(letra) {
+//Valida que el dato de entrada sea un numero.
+function validarNumero(letra) {             //¡Error: Admite espacios como entrada!!!
     let num = prompt ("Ingrese " + letra);
     while (isNaN(num)) {
         alert("Por favor ingrese un tipo de datos valido(NUMERO)");
@@ -11,49 +8,8 @@ function validarNumero(letra) {
     }
     return num
 }
-let a = validarNumero("a");
-let b = validarNumero("b");
-let c = validarNumero("c");
 
-//Calculo discriminante
-let disc = b * b - 4 * a * c;
-console.log (disc); // Comprobrar calculos
-
-//Inicializo
-let x1 = 0;
-let x2 = 0;
-
-
-switch (true) {
-    //Caso en el que hay dos raices reales 
-    case 0 < disc  :
-        x1 = (-b + Math.sqrt(disc)) / (2 * a);
-        x2 = (-b - Math.sqrt(disc)) / (2 * a); 
-        alert("Dos raices reales diferentes x1:" + x1.toFixed(2) + " y x2:" + x2.toFixed(2));            
-        break;
-    //Caso en el que hay solo una raiz real (raiz doble)
-    case disc == 0 :  
-        alert("Dos raices reales iguales: " + (-b / (2 * a)).toFixed(2));  
-        break;
-
-    case disc < 0 : // discriminante negativo
-    // Caso en el que hay dos raices complejas.
-        //Disc es negativo asi que se cambia el signo asi se puede calcular su raiz
-        disc = -disc;
-        x1 = -b / (2 * a);
-        alert("Dos raices complejas diferentes: " + x1.toFixed(2) + " (+/-) i " + (Math.sqrt(disc) / (2 * a)).toFixed(2));
-        break;
-    default: 
-        alert("Error");
-        break;
-}
-
-// isNAN(valor) devuelve true si valor es no numerico
-//isNaN() - if the value is a number, false is returned.
-//The Math.sqrt() static method returns the square root of a number. 
-
-
-//Historial
+//Inicializo array de historial
 const funcionesCalculadas = []; 
 //Objeto que guarda las raices calculadas
 class Funcion {
@@ -70,5 +26,87 @@ class Funcion {
 
     }
 }
+//Calculos precargados.
 funcionesCalculadas.push(new Funcion (1,2,3,"Imaginaria",1,2);)  
 const Funcion = new Funcion ("Imaginaria",1,2);
+//Programa principal.
+let salir = false;
+while not(salir) {
+    let opcion = prompt ("¿Que accion desea realizar?")
+    //Menu
+    switch (opcion) {
+        //1:Calcular nueva raiz.
+        //2: Display historial.
+        //3: Borrar 1 item del historial.
+        //:BOrrar solo los Reales?????s
+        //Borrar imaginaros??????
+        //4: Salir.
+        case 1:
+            alert("Calculadora de raices de un polinomio de 2do grado de la forma ax^2 +/- bx +/- c");
+            let a = validarNumero("a");
+            let b = validarNumero("b");
+            let c = validarNumero("c");
+
+            //Calculo discriminante
+            let disc = b * b - 4 * a * c;
+            console.log (disc); // Comprobrar calculos
+
+            //Inicializo
+            let x1 = 0;
+            let x2 = 0;
+
+
+            switch (true) {
+                //Caso en el que hay dos raices reales 
+                case 0 < disc  :
+                    x1 = (-b + Math.sqrt(disc)) / (2 * a);
+                    x2 = (-b - Math.sqrt(disc)) / (2 * a); 
+                    alert("Dos raices reales diferentes x1:" + x1.toFixed(2) + " y x2:" + x2.toFixed(2));            
+                    break;
+                //Caso en el que hay solo una raiz real (raiz doble)
+                case disc == 0 :  
+                    alert("Dos raices reales iguales: " + (-b / (2 * a)).toFixed(2));  
+                    break;
+
+                case disc < 0 : // discriminante negativo
+                // Caso en el que hay dos raices complejas.
+                    //Disc es negativo asi que se cambia el signo asi se puede calcular su raiz
+                    disc = -disc;
+                    x1 = -b / (2 * a);
+                    alert("Dos raices complejas diferentes: " + x1.toFixed(2) + " (+/-) i " + (Math.sqrt(disc) / (2 * a)).toFixed(2));
+                    break;
+                default: 
+                    alert("Error");
+                    break;
+            }
+
+            // isNAN(valor) devuelve true si valor es no numerico
+            //isNaN() - if the value is a number, false is returned.
+            //The Math.sqrt() static method returns the square root of a number. 
+
+            
+
+
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        default:
+            break;
+    }    
+}
+
+
+
+
+
+
+
+
+
